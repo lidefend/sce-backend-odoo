ca1ee11165ec1ea7f7be527ba05fab999b417f09
lidefend <307461631+lidefend@users.noreply.github.com>
2026-08-08 05:21:36 +0800

    fix(core): return runtime identity directly

--- addons/smart_core/controllers/runtime_version.py
+++ addons/smart_core/controllers/runtime_version.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
+import json
+
 from odoo import http
 from odoo.http import request
 
 from odoo.addons.smart_core.utils.product_release import runtime_release_identity
-
-from .platform_ops_controller import _ok
 
 
 class RuntimeVersionController(http.Controller):
@@ -17,4 +17,9 @@ class RuntimeVersionController(http.Controller):
         save_session=False,
     )
     def runtime_version(self, **_params):
-        return _ok(runtime_release_identity(request.env.cr.dbname))
+        payload = runtime_release_identity(request.env.cr.dbname)
+        return request.make_response(
+            json.dumps(payload, ensure_ascii=False),
+            headers=[("Content-Type", "application/json; charset=utf-8")],
+            status=200,
+        )
